fa81ebe40f65fc847279f82369da7a4669306764
Giacomo Schiavo <42863424+giacomoschiavo@users.noreply.github.com>
2025-04-05 11:59:14 +0200

    fixed testing notebook

--- dashboard/main_app.py
+++ dashboard/main_app.py
@@ -9,7 +9,8 @@ home_page = st.Page("homepage.py", title="Home", icon="🏠")
 models_page = st.Page("models_page.py", title="Models", icon="📊")
 species_page = st.Page("species_page.py", title="Species", icon="🔬")
 single_species_analysis = st.Page("single_species_analysis.py", title="Single Species Analysis", icon="🔍")
+dataset_audio_analysis = st.Page("dataset_audio_analysis.py", title="Dataset Audio Analysis", icon="🔍")
 
-pages = [home_page, models_page, species_page, single_species_analysis]
+pages = [home_page, models_page, species_page, single_species_analysis, dataset_audio_analysis]
 selected_page = st.navigation(pages)
 selected_page.run()
