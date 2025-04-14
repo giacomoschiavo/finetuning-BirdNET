e9322fe641edbd69151e37b55b577944b437b5d6
giacomoschiavo <giacomo.schiavo12@gmail.com>
2025-04-14 10:55:27 +0000

    changed models structure

---
+++ models/VanillaCNN/model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+class VanillaCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(AudioCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, 2)
+
+        # Adaptive pooling: forza le feature map a 4x4
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))  # output shape: [batch, 64, 4, 4]
+
+        self.fc1 = nn.Linear(64 * 4 * 4, 128)
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        if x.ndim == 3:  # [batch, 256, 256] → aggiungi il canale
+            x = x.unsqueeze(1)
+        
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+
+        x = self.adaptive_pool(x)  # [B, 64, 4, 4]
+        x = x.view(x.size(0), -1)  # Flatten → [B, 1024]
+
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
