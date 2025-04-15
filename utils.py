d8a82112ec20777b9880246d9c0243cb4041e983
giacomoschiavo <giacomo.schiavo12@gmail.com>
2025-04-15 10:20:09 +0000

    moved functions to utils

--- utils.py
+++ utils.py
@@ -3,6 +3,86 @@ import torchaudio
 import torch
 import torch.nn.functional as F
 import random
+from torch.utils.data import DataLoader, Dataset
+import importlib
+
+def load_model_class(model_name):
+    model_module = importlib.import_module(f"models.{model_name}.model")
+    model_class = getattr(model_module, model_name)
+    return model_class
+
+def collect_samples(train_path, test_path, mappings):
+    samples_train = {}
+    for species in os.listdir(train_path):
+        if species not in os.listdir(test_path):
+            continue
+        audio_list = os.listdir(os.path.join(train_path, species))
+        valid_samples = random.sample(audio_list, len(audio_list) // 10)
+        for audio in audio_list:
+            if audio in samples_train:      # same audio in different folders, save the other species
+                samples_train[audio]["labels"].append(mappings[species])
+                continue
+            samples_train[audio] = {
+                "file_path": os.path.join(train_path, species, audio),
+                "split": "valid" if audio in valid_samples else "train",
+                "labels": [mappings[species]]
+            }
+
+    samples_test = {}
+    for species in os.listdir(test_path):
+        for audio in os.listdir(os.path.join(test_path, species)):
+            labels = [mappings[species]]
+            updated = False
+            if audio in samples_test:       # adds label
+                samples_test[audio]["labels"].extend(labels)
+                updated = True
+            if audio in samples_train:      # considers training labels
+                samples_train[audio]["labels"].extend(labels)       # add test label
+                labels = samples_train[audio]["labels"]             # save new labels
+            if updated:
+                continue
+            samples_test[audio] = {
+                "file_path": os.path.join(test_path, species, audio),
+                "split": "test",
+                "labels": labels
+            }
+    samples = []
+    for _, props in samples_train.items():
+        samples.append(props)
+    for _, props in samples_test.items():
+        samples.append(props)
+    return samples
+
+def wav_to_spec(audio_path):
+    split = audio_path.split("/")[-3]
+    audio = audio_path.replace("wav", "pt").replace(split, f"{split}_specs")
+    return audio
+
+class CachedAudioDataset(Dataset):
+    def __init__(self, dataset_config, split="train"):
+        self.samples = [s for s in dataset_config["samples"] if s["split"] == split]
+        self.num_classes = len(dataset_config["mappings"])
+        
+        # Carica tutto in RAM
+        self.cache = []
+        for sample in self.samples:
+            spec = torch.load(wav_to_spec(sample["file_path"]))  # spettrogramma
+            label_tensor = torch.zeros(self.num_classes)
+            for label in sample["labels"]:
+                label_tensor[label] = 1.0
+            self.cache.append((spec, label_tensor, sample["file_path"]))
+
+    def __len__(self):
+        return len(self.cache)
+
+    def __getitem__(self, idx):
+        return self.cache[idx]
+
+
+def get_dataloader(dataset_config, split="train", batch_size=100, shuffle=True):
+    dataset = CachedAudioDataset(dataset_config=dataset_config, split=split)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+
 
 def specs_generation(input_path, output_path, mappings):
     sample_rate = 32000
@@ -57,52 +137,9 @@ def specs_generation(input_path, output_path, mappings):
             # spectrogram = (spectrogram - spectrogram.min()) / (spectrogram.max() - spectrogram.min())
 
             # Resize to 256x256
-            spectrogram = spectrogram.unsqueeze(0).unsqueeze(0)  # (1, 1, freq, time)
-            print(audio)
+            spectrogram = spectrogram.unsqueeze(0) # (1, 1, freq, time)
             spectrogram = F.interpolate(spectrogram, size=(256, 256), mode="bilinear", align_corners=False)
             spectrogram = spectrogram.squeeze(0).squeeze(0)  # torna a (256, 256)
 
             # Save the spectrogram tensor
             torch.save(spectrogram, save_path)
-
-def collect_samples(train_path, test_path, mappings):
-    samples_train = {}
-    for species in os.listdir(train_path):
-        if species not in os.listdir(test_path):
-            continue
-        audio_list = os.listdir(os.path.join(train_path, species))
-        valid_samples = random.sample(audio_list, len(audio_list) // 10)
-        for audio in audio_list:
-            if audio in samples_train:      # same audio in different folders, save the other species
-                samples_train[audio]["labels"].append(mappings[species])
-                continue
-            samples_train[audio] = {
-                "file_path": os.path.join(train_path, species, audio),
-                "split": "valid" if audio in valid_samples else "train",
-                "labels": [mappings[species]]
-            }
-
-    samples_test = {}
-    for species in os.listdir(test_path):
-        for audio in os.listdir(os.path.join(test_path, species)):
-            labels = [mappings[species]]
-            updated = False
-            if audio in samples_test:       # adds label
-                samples_test[audio]["labels"].extend(labels)
-                updated = True
-            if audio in samples_train:      # considers training labels
-                samples_train[audio]["labels"].extend(labels)       # add test label
-                labels = samples_train[audio]["labels"]             # save new labels
-            if updated:
-                continue
-            samples_test[audio] = {
-                "file_path": os.path.join(test_path, species, audio),
-                "split": "test",
-                "labels": labels
-            }
-    samples = []
-    for _, props in samples_train.items():
-        samples.append(props)
-    for _, props in samples_test.items():
-        samples.append(props)
-    return samples
