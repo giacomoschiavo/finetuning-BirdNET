67e1b70c50fd5737ab7722e8d48af63860bb3eb3
giacomoschiavo <giacomo.schiavo12@gmail.com>
2025-05-02 15:29:55 +0000

    true_segments divided in train, test e full

--- exec/build_dataset.py
+++ exec/build_dataset.py
@@ -1,7 +1,7 @@
 import json
 import os
 import argparse
-
+import copy
 import sys
 sys.path.append('../')
 from birdlib import utils
@@ -53,17 +53,21 @@ def main(args):
     audio_info = load_or_generate_info("audio_info.json", audio_annots, args.source, utils_path)
     audio_info_test = load_or_generate_info("audio_info_test.json", audio_annots_test, args.source, utils_path)
 
-    true_segments = utils.generate_true_segments(audio_annots, audio_info)
+    true_segments_train = utils.generate_true_segments(audio_annots, audio_info)
     true_segments_test = utils.generate_true_segments(audio_annots_test, audio_info_test)
+
+    true_segments = copy.deepcopy(true_segments_train)
+    true_segments.update(true_segments_test)
     print("Created True Segments")
 
-    save_json(true_segments, os.path.join(args.meta_path, args.dataset_name, "true_segments.json"))
+    save_json(true_segments_train, os.path.join(args.meta_path, args.dataset_name, "true_segments_train.json"))
     save_json(true_segments_test, os.path.join(args.meta_path, args.dataset_name, "true_segments_test.json"))
+    save_json(true_segments, os.path.join(args.meta_path, args.dataset_name, "true_segments.json"))
 
     output_path = args.output_path or f"./{args.dataset_name}"
 
     print("Generating segments...")
-    utils.generate_segments(args.source, f"{output_path}/train", true_segments, audio_info, generate_None=True)
+    utils.generate_segments(args.source, f"{output_path}/train", true_segments_train, audio_info, generate_None=True)
     utils.generate_segments(args.source, f"{output_path}/test", true_segments_test, audio_info_test, generate_None=True)
     print("Done!")
 
