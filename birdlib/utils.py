f469765344c3390ddf7177b206dcbae0054d6af5
giacomoschiavo <giacomo.schiavo12@gmail.com>
2025-05-03 16:46:00 +0000

    fixed preprocessing

--- birdlib/utils.py
+++ birdlib/utils.py
@@ -12,15 +12,10 @@ import librosa
 from pydub import AudioSegment
 from tqdm import tqdm
 
-def get_mappings(train_path, include_None=False):
+def get_mappings(train_path):
     train_species = os.listdir(train_path) 
-    with open("utils/category_annots.json") as f:
-        category_annots = json.load(f)
-
-    filtered_species = [species for species in category_annots.keys() if species in train_species]
+    filtered_species = [species for species in train_species]
     mappings = {species: i for i, species in enumerate(filtered_species)}
-    if include_None:
-        mappings['None'] = len(filtered_species)
     return mappings
 
 def load_model_class(model_name):
@@ -28,22 +23,50 @@ def load_model_class(model_name):
     model_class = getattr(model_module, model_name)
     return model_class
 
-def collect_samples(train_path, test_path, final_test_path, mappings, seed=42):
-    random.seed(seed)  
+def collect_samples(train_path, valid_path, test_path, mappings):
+    # {
+    #     "file_path": "Aeroplane/20190621_070000_472_5.wav",
+    #     "split": "train",
+    #     "labels": [
+    #         16,
+    #         2
+    #     ]
+    # },
     samples_train = {}
     for species in os.listdir(train_path):
         if species not in mappings:
             continue
         audio_list = os.listdir(os.path.join(train_path, species))
-        valid_samples = random.sample(audio_list, len(audio_list) // 10)
         for audio in audio_list:
             if audio in samples_train:      # same audio in different folders, save the other species
                 samples_train[audio]["labels"].append(mappings[species])
-                continue
+                continue                    # do not save another sample of the same audio
             samples_train[audio] = {
                 "file_path": os.path.join(train_path, species, audio),
-                "split": "valid" if audio in valid_samples else "train",
+                "split": "train",
                 "labels": [mappings[species]]
+            }
+
+
+    samples_valid = {}
+    for species in os.listdir(valid_path):
+        if species not in mappings:
+            continue
+        for audio in os.listdir(os.path.join(valid_path, species)):
+            labels = [mappings[species]]
+            updated = False
+            if audio in samples_valid:       # adds label
+                samples_valid[audio]["labels"].extend(labels)
+                updated = True
+            if audio in samples_train:      # considers training labels
+                samples_train[audio]["labels"].extend(labels)       # add test label
+                labels = samples_train[audio]["labels"]             # save new labels
+            if updated:
+                continue
+            samples_valid[audio] = {
+                "file_path": os.path.join(valid_path, species, audio),
+                "split": "valid",
+                "labels": labels
             }
 
     samples_test = {}
@@ -59,6 +82,9 @@ def collect_samples(train_path, test_path, final_test_path, mappings, seed=42):
             if audio in samples_train:      # considers training labels
                 samples_train[audio]["labels"].extend(labels)       # add test label
                 labels = samples_train[audio]["labels"]             # save new labels
+            if audio in samples_valid:      # considers training labels
+                samples_valid[audio]["labels"].extend(labels)       # add test label
+                labels = samples_valid[audio]["labels"]             # save new labels
             if updated:
                 continue
             samples_test[audio] = {
@@ -67,25 +93,14 @@ def collect_samples(train_path, test_path, final_test_path, mappings, seed=42):
                 "labels": labels
             }
 
-    samples_final_test = {}
-    for species in os.listdir(final_test_path):
-        if species not in mappings:
-            continue
-        for audio in os.listdir(os.path.join(final_test_path, species)):
-            labels = [mappings[species]]
-            samples_final_test[audio] = {
-                "file_path": os.path.join(final_test_path, species, audio),
-                "split": "final_test",
-                "labels": labels
-            }
-
     samples = []
     for _, props in samples_train.items():
         samples.append(props)
+    for _, props in samples_valid.items():
+        samples.append(props)
     for _, props in samples_test.items():
         samples.append(props)
-    for _, props in samples_final_test.items():
-        samples.append(props)
+
     return samples
 
 def wav_to_spec(audio_path):
