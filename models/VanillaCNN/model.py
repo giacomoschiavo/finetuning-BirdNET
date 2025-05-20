9bf88d7f0d960ba5436ae9597ae2fc70e5ff2018
giacomoschiavo <giacomo.schiavo12@gmail.com>
2025-05-20 10:56:18 +0000

    add new models, new training pipeline

--- models/VanillaCNN/model.py
+++ models/VanillaCNN/model.py
@@ -3,30 +3,24 @@ import torch.nn as nn
 class VanillaCNN(nn.Module):
     def __init__(self, num_classes):
         super(VanillaCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
 
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2, 2)
 
-        # Adaptive pooling: forza le feature map a 4x4
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((4, 4))  # output shape: [batch, 64, 4, 4]
-
-        self.fc1 = nn.Linear(64 * 4 * 4, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(128 * 32 * 32, num_classes) 
 
     def forward(self, x):
-        if x.ndim == 3:  # [batch, 256, 256] → aggiungi il canale
+        if x.ndim == 3:  # [batch, H, W] aggiungi il canale
             x = x.unsqueeze(1)
-        
+
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
         x = self.pool(self.relu(self.conv3(x)))
+        
 
-        x = self.adaptive_pool(x)  # [B, 64, 4, 4]
-        x = x.view(x.size(0), -1)  # Flatten → [B, 1024]
-
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = x.view(x.size(0), -1)  # Flatten
+        x = self.fc1(x)
         return x
